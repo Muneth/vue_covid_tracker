78c40b2ccdd5c6a6872004548c7479450e07a720
muneet <muneetchaudhary@me.com>
2021-11-01 13:23:32 +0100

    initial commit

---
+++ src/components/Bar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="p-10">
+    <svg height="530" width="1400">
+      <g transform="rotate(180)" fill="none" stroke="black" stroke-width="0">
+        <rect
+          width="90"
+          :height="stats.NewConfirmed / 1000"
+          style="fill:rgb(194, 189, 41)"
+          x="-350"
+          y="-500"
+        ></rect>
+        <rect
+          width="90"
+          :height="stats.TotalConfirmed / 1000"
+          style="fill:rgb(156, 151, 6)"
+          x="-500"
+          y="-500"
+        ></rect>
+        <rect
+          width="90"
+          :height="stats.NewRecovered / 1000"
+          style="fill:rgb(87, 121, 235)"
+          x="-650"
+          y="-500"
+        ></rect>
+        <rect
+          width="90"
+          :height="stats.TotalRecovered / 1000"
+          style="fill:rgb(9, 51, 148)"
+          x="-800"
+          y="-500"
+        ></rect>
+        <rect
+          width="90"
+          :height="stats.NewDeaths / 1000"
+          style="fill:rgb(227, 0, 0)"
+          x="-950"
+          y="-500"
+        ></rect>
+        <rect
+          width="90"
+          :height="stats.TotalDeaths / 1000"
+          style="fill:rgb(15, 0, 2)"
+          x="-1100"
+          y="-500"
+        ></rect>
+      </g>
+      <text x="260" y="530" fill="grey">New Cases</text>
+      <text x="410" y="530" fill="grey">Total Cases</text>
+      <text x="550" y="530" fill="grey">New Recovered</text>
+      <text x="700" y="530" fill="grey">Total Recovered</text>
+      <text x="860" y="530" fill="grey">New Deaths</text>
+      <text x="1010" y="530" fill="grey">Total Deaths</text>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Bar",
+  props: ["stats"],
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,82 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="mb-10" v-if="!loading">
+    <div class="my-4">
+      <country-select
+        @get-country="getCountryData"
+        :countries="countries"
+      ></country-select>
+      <button
+        @click="clearCountryData"
+        v-if="stats.Country"
+        class="bg-green-700 text-white rounded p-3 my-4 focus:ouline-none hover:bg-green-600"
+      >
+        Clear Country
+      </button>
+    </div>
+    <Bar :stats="stats" />
+    <!-- TWO DIFFERENT WAYS TO DECLARE Components -->
+    <!-- <data-title :text="title" :dataDate="dataDate"></data-title> -->
+    <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+  </main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="" />
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// Differnt WAYS TO Import Components...
+
+import DataTitle from "@/components/DataTitle";
+import DataBoxes from "@/components/DataBoxes";
+import Bar from "@/components/Bar";
+import CountrySelect from "../components/CountrySelect.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    DataTitle,
+    DataBoxes,
+    CountrySelect,
+    Bar,
+  },
+  data() {
+    DataTitle;
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = "Global";
+      this.stats = data.Global;
+      this.loading = false;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
+};
 </script>
